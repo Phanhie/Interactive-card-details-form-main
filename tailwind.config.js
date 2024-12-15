41d17e0bd3837f0d121a902c675289670e0ef837
Phanhie <yangestepanied@gmail.com>
2024-12-15 21:27:16 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+        '8h': '730px'
+      },
+      width: {
+        '4h': '420px'
+      },
+      colors:{
+        'd-violet': 'hsl(278, 68%, 11%)'
+      },
+      fontFamily:{
+        'display': ['Space Grotesk']
+      }
+    },
   },
   plugins: [],
 }

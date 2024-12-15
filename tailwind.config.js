ae8b376126d7ab0ff29fbaf9fa2df4592d627c0c
Phanhie <yangestepanied@gmail.com>
2024-12-15 22:29:24 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,18 @@ module.exports = {
         '8h': '730px'
       },
       width: {
-        '4h': '420px'
+        '4h': '500px'
       },
       colors:{
         'd-violet': 'hsl(278, 68%, 11%)'
       },
       fontFamily:{
         'display': ['Space Grotesk']
-      }
-    },
+      },
+      backgroundImage: {
+        'cardFront': "url('src/images/bg-card-front.png')",
+    }
   },
   plugins: [],
+ }
 }
-
